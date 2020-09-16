42a5064f8905a4f41df08b8771bffe6bd6365c08
Boning Yang <sylvartore@gmail.com>
2020-09-16 15:18:53 -0700

    add card class

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-
-def get_hash(img):
-    image = img.resize((18, 13), Image.ANTIALIAS).convert("L")
-    pixels = list(image.getdata())
-    avg = sum(pixels)/len(pixels)
-    return "".join(map(lambda p: "1" if p > avg else "0"), pixels)
